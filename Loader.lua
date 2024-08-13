d0caee60d9d7254712266c6d6b5770ff4d95ef54
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-08-13 21:41:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,19 +1,6 @@
 if game.PlaceId == 6897167394 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/FruitSea.lua"))()
-elseif game.PlaceId == 10260193230 then
-
-spawn(function()
-    while wait() do
-        pcall(function()
-            if game.Players.LocalPlayer.PlayerGui.LoadingGui.Enabled == true then
-                for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.LoadingGui.PlayBackground.Play.Activated)) do
-                    v.Function()
-                end
-            end
-        end)
-    end
-end)
-    
+elseif game.PlaceId == 10260193230 then    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/MemeSea.lua"))()
 elseif game.PlaceId == 17610616912 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/AnimePowerEvolutionSimulator.lua"))()
