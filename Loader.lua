882da507a119a6a433ed9fac21591278ec01eae6
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-08-14 18:07:17 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/executelog-obfuscated.lua"))()
+
 if game.PlaceId == 6897167394 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/FruitSea.lua"))()
 elseif game.PlaceId == 10260193230 then    
