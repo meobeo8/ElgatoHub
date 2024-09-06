2c3297f304899244010bebb0070589570ae32436
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-09-06 18:34:24 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,6 +4,4 @@ elseif game.PlaceId == 10260193230 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/MemeSea.lua"))()
 elseif game.PlaceId == 18688206652 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LjulPiece2.lua"))()
-elseif game.PlaceId == 17610616912 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/AnimePowerEvolutionSimulator.lua"))()
 end
