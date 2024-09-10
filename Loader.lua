c18e7e400e9006dad1ad5e7cd1f2e40c2c317317
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-09-10 15:19:48 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,7 +1,9 @@
 if game.PlaceId == 6897167394 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/FruitSea.lua"))()
 elseif game.PlaceId == 10260193230 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/MemeSea.lua"))()
+  --  loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/MemeSea.lua"))()
+    game.Players.LocalPlayer:Kick("MAINTENANCE/BẢO TRÌ")
 elseif game.PlaceId == 18688206652 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LjulPiece2.lua"))()
+ --   loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LjulPiece2.lua"))()
+    game.Players.LocalPlayer:Kick("MAINTENANCE/BẢO TRÌ")
 end
