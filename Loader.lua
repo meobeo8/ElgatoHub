93d428e3808865e9c94ce98c1e57eabb6617e000
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-09-26 12:07:47 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -4,4 +4,6 @@ elseif game.PlaceId == 10260193230 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/MemeSea.lua"))()
 elseif game.PlaceId == 18688206652 then
    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LjulPiece2.lua"))()
+elseif game.PlaceId == 16448761503 then
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/RungSea.lua"))()
 end
