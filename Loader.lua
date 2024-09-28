d18abf3cadb2f7c25a03ea23813b1f0ce951be19
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-09-28 18:28:54 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,18 @@
-if game.PlaceId == 6897167394 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/FruitSea.lua"))()
-elseif game.PlaceId == 10260193230 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/MemeSea.lua"))()
-elseif game.PlaceId == 18688206652 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LjulPiece2.lua"))()
-elseif game.PlaceId == 16448761503 then
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/RungSea.lua"))()
+local Place = game.PlaceId
+local Game = game.GameId
+local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
+
+local scripts = {
+    [16448761503] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/RungSea.lua",
+    [18688206652] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/LjulPiece2.lua",
+    [10260193230] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/MemeSea.lua"
+}
+
+local scriptURL = scripts[Place] or scripts[Game]
+
+if scriptURL then
+    print(GameName)
+    loadstring(game:HttpGet(scriptURL))()
+else
+    warn("No script found for this game.")
 end
