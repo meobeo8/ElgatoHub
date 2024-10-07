63777e3bb54195b6f6db385f84514fdf494a840b
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-10-07 10:16:40 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,20 +2,24 @@ local Place = game.PlaceId
 local Game = game.GameId
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
 
-local scripts = {
-    [16448761503] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/RungSea.lua",
-    [18688206652] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/LjulPiece2.lua",
-    [18968803452] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/MuyongPiece.lua",
-    [75972208642796] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/JoodPiece2.lua",
-    [86639052909924] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/VersePiece2.lua",
-    [10260193230] = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/MemeSea.lua"
-}
-
-local scriptURL = scripts[Place] or scripts[Game]
-
-if scriptURL then
+if Place == 16448761503 then
     print(GameName)
-    loadstring(game:HttpGet(scriptURL))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/RungSea.lua"))()
+elseif Place == 18688206652 then
+    print(GameName)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/LjulPiece2.lua"))()
+elseif Place == 18968803452 then
+    print(GameName)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/MuyongPiece.lua"))()
+elseif Place == 75972208642796 then
+    print(GameName)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/JoodPiece2.lua"))()
+elseif Place == 86639052909924 then
+    print(GameName)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/VersePiece2.lua"))()
+elseif Place == 10260193230 then
+    print(GameName)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/MemeSea.lua"))()
 else
     game.Players.LocalPlayer:Kick("This game is not supported or has been removed.")
 end
