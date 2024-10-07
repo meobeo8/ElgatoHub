4b9af471555e8e6fd8ac5349d540105a38b269de
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-10-07 14:38:51 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,22 +1,36 @@
+function Notify1(va, va1)
+    game.StarterGui:SetCore("SendNotification", {
+        Title = "elgato hihi",
+        Text = va,
+        Icon = "",
+        Duration = va1,
+        Button1 = "Okay",
+    })
+end
+
+function Notify(va)
+    Notify1(va, 5)
+end
+
+Notify("Use Fluxus as it supports best")
+
 local Place = game.PlaceId
-local Game = game.GameId
 local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
 
-if Place == 16448761503 then
-    print(GameName)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/RungSea.lua"))()
-elseif Place == 18688206652 then
-    print(GameName)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/LjulPiece2.lua"))()
-elseif Place == 75972208642796 then
-    print(GameName)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/JoodPiece2.lua"))()
-elseif Place == 86639052909924 then
-    print(GameName)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/VersePiece2.lua"))()
-elseif Place == 10260193230 then
-    print(GameName)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/refs/heads/main/MemeSea.lua"))()
+local baseURL = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/"
+
+local placeScripts = {
+    [16448761503] = "RungSea.lua",
+    [18688206652] = "LjulPiece2.lua",
+    [10260193230] = "MemeSea.lua",
+    [75972208642796] = "JoodPiece2.lua",
+    [86639052909924] = "VersePiece2.lua"
+}
+
+local scriptName = placeScripts[Place]
+if scriptName then
+    Notify(GameName)
+    loadstring(game:HttpGet(baseURL .. scriptName))()
 else
     game.Players.LocalPlayer:Kick("This game is not supported or has been removed.")
 end
