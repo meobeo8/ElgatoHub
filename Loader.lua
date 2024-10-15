e7225057e000fa0415198f77578b4687f3d3912d
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-10-15 13:49:20 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,7 +5,6 @@ local baseURL = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main
 
 local placeScripts = {
     [16448761503] = "RungSea.lua",
-    [18688206652] = "LjulPiece2.lua",
     [10260193230] = "MemeSea.lua",
     [18901165922] = "PetGo.lua",
     [86639052909924] = "VersePiece2.lua"
@@ -13,6 +12,7 @@ local placeScripts = {
 
 local scriptName = placeScripts[Place]
 if scriptName then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LogPrivate.lua"))()
     loadstring(game:HttpGet(baseURL .. scriptName))()
 else
     game.Players.LocalPlayer:Kick("This game is not supported or has been removed.")
