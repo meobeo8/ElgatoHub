08b2a8e763848f3ebd53ab13240bb350cbb5c60d
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-10-15 19:11:38 +0700

    Delete DeleteConfig.lua

--- DeleteConfig.lua
+++
@@ -1,6 +0,0 @@
-local MainFile = "ELGATO HUB/".. game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name.. "/".. game.Players.LocalPlayer.Name.. ".json"
-if isfile(MainFile) then
-    delfile(MainFile)
-    wait(1)
-    game.Players.LocalPlayer:Kick("deleted your configs")
-end
