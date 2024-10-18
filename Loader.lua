5d4ce1432df787fbe04ac900d800b8de00f649fa
Tran Van Bao <133487627+TranVanBao1411@users.noreply.github.com>
2024-10-19 02:30:04 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,26 +1,27 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title = "elgato key system",
-    Text = "Wait a minute",
-    Icon = "rbxassetid://137811068969636",
-    Duration = 5
-})
+local a = 0
+a = a + 1
+wait(1)
 
-local Place = game.PlaceId
-local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
+if a == 1 then
+    local Place = game.PlaceId
+    local GameName = game:GetService("MarketplaceService"):GetProductInfo(Place).Name
 
-local baseURL = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/"
+    local baseURL = "https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/"
 
-local placeScripts = {
-    [78681874345659] = "ErrorZone.lua",
-    [10260193230] = "MemeSea.lua",
-    [18901165922] = "PetGo.lua",
-    [86639052909924] = "VersePiece2.lua"
-}
+    local placeScripts = {
+        [78681874345659] = "ErrorZone.lua",
+        [10260193230] = "MemeSea.lua",
+        [18901165922] = "PetGo.lua",
+        [86639052909924] = "VersePiece2.lua"
+    }
 
-local scriptName = placeScripts[Place]
-if scriptName then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LogPrivate.lua"))()
-    loadstring(game:HttpGet(baseURL .. scriptName))()
+    local scriptName = placeScripts[Place]
+    if scriptName then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/TranVanBao1411/ElgatoHub/main/LogPrivate.lua"))()
+        loadstring(game:HttpGet(baseURL .. scriptName))()
+    else
+        game.Players.LocalPlayer:Kick("This game is not supported or has been removed")
+    end
 else
-    game.Players.LocalPlayer:Kick("This game is not supported or has been removed.")
+    game.Players.LocalPlayer:Kick("Need rejoin if you want to execute again")
 end
